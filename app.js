54f37d611a2847adf653d445c64d8dfdcedd5f2d
Benedicte Naess <144252770+benedictenaess@users.noreply.github.com>
2023-11-22 13:59:27 +0100

    Append text to container

--- app.js
+++ app.js
@@ -3,11 +3,23 @@ const submitButton = document.querySelector('.submit-button');
 const notesContainer = document.querySelector('.notes-container');
 const errorMessage = document.querySelector('.error-message');
 
+const colors = ['white', 'lightpink', 'coral', 'hotpink', 'lavender', 'lightblue', 'indianred', 'peru', 'cold' ,'yellow'];
+
+
+
 const handleNotes = () => {
 	if (userNote.value.trim() === ''){
 		errorMessage.style.display = 'block';
 		return
 	}
+
+	const randomColor = Math.floor(Math.random() * (10 - 0) + 0);
+	const randomRotation = Math.floor(Math.random() * (11 - -10) + - 10);
+
+	const stickyDiv = document.createElement('div');
+	notesContainer.append(stickyDiv);
+	stickyDiv.textContent = userNote.value;
+	
 }
 
 submitButton.addEventListener('click', handleNotes);
