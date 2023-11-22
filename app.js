8e1ff32160fd2daeebe12b4e89e21cf1024c0c08
Benedicte Naess <144252770+benedictenaess@users.noreply.github.com>
2023-11-22 15:01:08 +0100

    User note input done

--- app.js
+++ app.js
@@ -3,13 +3,23 @@ const submitButton = document.querySelector('.submit-button');
 const notesContainer = document.querySelector('.notes-container');
 const errorMessage = document.querySelector('.error-message');
 
-const colors = ['white', 'lightpink', 'coral', 'hotpink', 'lavender', 'lightblue', 'indianred', 'peru', 'cold' ,'yellow'];
+const colors = ['white', 'lightpink', 'coral', 'hotpink', 'lavender', 'lightblue', 'indianred', 'peru', 'lightgreen' ,'lightpurple'];
 
 
+userNote.addEventListener('input', () => {
+	errorMessage.style.display = 'none';
+	if (userNote.value.length >= 200){
+		errorMessage.style.display = 'block';
+		errorMessage.textContent = 'You have reached the maximum characters allowed';
+		return;
+	}
+	errorMessage.textContent = 'Please enter text';
+})
 
 const handleNotes = () => {
 	if (userNote.value.trim() === ''){
 		errorMessage.style.display = 'block';
+		errorMessage.textContent = 'Please enter text';
 		return
 	}
 
@@ -19,7 +29,13 @@ const handleNotes = () => {
 	const stickyDiv = document.createElement('div');
 	notesContainer.append(stickyDiv);
 	stickyDiv.textContent = userNote.value;
-	
+	stickyDiv.classList.add('sticky-div');
+	stickyDiv.style.backgroundColor = colors[randomColor];
+	stickyDiv.style.transform = `rotate(${randomRotation}deg)`	
+
+	userNote.value = '';
 }
+
+
 
 submitButton.addEventListener('click', handleNotes);
